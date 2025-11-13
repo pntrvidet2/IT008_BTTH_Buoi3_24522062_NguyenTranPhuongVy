e670b65715f1e664e57120eaeb8b85a2dbe5a08e
24522062_NguyenTranPhuongVy <24522062@gm.uit.edu.vn>
2025-11-13 22:24:03 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace BT09
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            txtMaSV = new TextBox();
+            txtHoTen = new TextBox();
+            cbChuyenNganh = new ComboBox();
+            listBoxMonHoc1 = new ListBox();
+            listBoxMonHoc2 = new ListBox();
+            btnChuyenPhai = new Button();
+            btnChuyenTrai = new Button();
+            groupBoxThongTin = new GroupBox();
+            rbNu = new RadioButton();
+            rbNam = new RadioButton();
+            btnXoa = new Button();
+            btnLuu = new Button();
+            dGVThongTin = new DataGridView();
+            MaSV = new DataGridViewTextBoxColumn();
+            HoTen = new DataGridViewTextBoxColumn();
+            ChuyenNganh = new DataGridViewTextBoxColumn();
+            GioiTinh = new DataGridViewTextBoxColumn();
+            SoMon = new DataGridViewTextBoxColumn();
+            MonHoc = new DataGridViewTextBoxColumn();
+            groupBoxThongTin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dGVThongTin).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(155, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Mã Sinh Viên";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(155, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Họ Tên";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(155, 90);
+            label3.Name = "label3";
+            label3.Size = new Size(105, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Chuyên Ngành";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(155, 123);
+            label4.Name = "label4";
+            label4.Size = new Size(68, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Giới Tính";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(155, 157);
+            label5.Name = "label5";
+            label5.Size = new Size(203, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Chọn Các Môn Học Tham Gia";
+            // 
+            // txtMaSV
+            // 
+            txtMaSV.Location = new Point(285, 24);
+            txtMaSV.Name = "txtMaSV";
+            txtMaSV.Size = new Size(235, 27);
+            txtMaSV.TabIndex = 5;
+            // 
+            // txtHoTen
+            // 
+            txtHoTen.Location = new Point(285, 59);
+            txtHoTen.Name = "txtHoTen";
+            txtHoTen.Size = new Size(235, 27);
+            txtHoTen.TabIndex = 6;
+            // 
+            // cbChuyenNganh
+            // 
+            cbChuyenNganh.FormattingEnabled = true;
+            cbChuyenNganh.Location = new Point(285, 92);
+            cbChuyenNganh.Name = "cbChuyenNganh";
+            cbChuyenNganh.Size = new Size(235, 28);
+            cbChuyenNganh.TabIndex = 7;
+            // 
+            // listBoxMonHoc1
+            // 
+            listBoxMonHoc1.FormattingEnabled = true;
+            listBoxMonHoc1.Location = new Point(155, 180);
+            listBoxMonHoc1.Name = "listBoxMonHoc1";
+            listBoxMonHoc1.SelectionMode = SelectionMode.MultiExtended;
+            listBoxMonHoc1.Size = new Size(235, 104);
+            listBoxMonHoc1.TabIndex = 11;
+            // 
+            // listBoxMonHoc2
+            // 
+            listBoxMonHoc2.FormattingEnabled = true;
+            listBoxMonHoc2.Location = new Point(469, 180);
+            listBoxMonHoc2.Name = "listBoxMonHoc2";
+            listBoxMonHoc2.SelectionMode = SelectionMode.MultiExtended;
+            listBoxMonHoc2.Size = new Size(211, 104);
+            listBoxMonHoc2.TabIndex = 12;
+            // 
+            // btnChuyenPhai
+            // 
+            btnChuyenPhai.BackColor = SystemColors.AppWorkspace;
+            btnChuyenPhai.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnChuyenPhai.Location = new Point(413, 197);
+            btnChuyenPhai.Name = "btnChuyenPhai";
+            btnChuyenPhai.Size = new Size(40, 29);
+            btnChuyenPhai.TabIndex = 13;
+            btnChuyenPhai.Text = ">";
+            btnChuyenPhai.UseVisualStyleBackColor = false;
+            btnChuyenPhai.Click += btnChuyenPhai_Click;
+            // 
+            // btnChuyenTrai
+            // 
+            btnChuyenTrai.BackColor = SystemColors.AppWorkspace;
+            btnChuyenTrai.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnChuyenTrai.Location = new Point(413, 232);
+            btnChuyenTrai.Name = "btnChuyenTrai";
+            btnChuyenTrai.Size = new Size(41, 29);
+            btnChuyenTrai.TabIndex = 14;
+            btnChuyenTrai.Text = "<";
+            btnChuyenTrai.UseVisualStyleBackColor = false;
+            btnChuyenTrai.Click += btnChuyenTrai_Click;
+            // 
+            // groupBoxThongTin
+            // 
+            groupBoxThongTin.AutoSize = true;
+            groupBoxThongTin.Controls.Add(rbNu);
+            groupBoxThongTin.Controls.Add(rbNam);
+            groupBoxThongTin.Controls.Add(btnXoa);
+            groupBoxThongTin.Controls.Add(btnLuu);
+            groupBoxThongTin.Controls.Add(btnChuyenTrai);
+            groupBoxThongTin.Controls.Add(btnChuyenPhai);
+            groupBoxThongTin.Controls.Add(listBoxMonHoc2);
+            groupBoxThongTin.Controls.Add(listBoxMonHoc1);
+            groupBoxThongTin.Controls.Add(cbChuyenNganh);
+            groupBoxThongTin.Controls.Add(txtHoTen);
+            groupBoxThongTin.Controls.Add(txtMaSV);
+            groupBoxThongTin.Controls.Add(label5);
+            groupBoxThongTin.Controls.Add(label4);
+            groupBoxThongTin.Controls.Add(label3);
+            groupBoxThongTin.Controls.Add(label2);
+            groupBoxThongTin.Controls.Add(label1);
+            groupBoxThongTin.Location = new Point(0, 0);
+            groupBoxThongTin.Name = "groupBoxThongTin";
+            groupBoxThongTin.Size = new Size(788, 342);
+            groupBoxThongTin.TabIndex = 0;
+            groupBoxThongTin.TabStop = false;
+            groupBoxThongTin.Text = "Thông Tin Sinh Viên";
+            groupBoxThongTin.Enter += groupBoxThongTin_Enter;
+            // 
+            // rbNu
+            // 
+            rbNu.AutoSize = true;
+            rbNu.Location = new Point(421, 121);
+            rbNu.Name = "rbNu";
+            rbNu.Size = new Size(50, 24);
+            rbNu.TabIndex = 19;
+            rbNu.TabStop = true;
+            rbNu.Text = "Nữ";
+            rbNu.UseVisualStyleBackColor = true;
+            // 
+            // rbNam
+            // 
+            rbNam.AutoSize = true;
+            rbNam.Location = new Point(285, 123);
+            rbNam.Name = "rbNam";
+            rbNam.Size = new Size(62, 24);
+            rbNam.TabIndex = 18;
+            rbNam.TabStop = true;
+            rbNam.Text = "Nam";
+            rbNam.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            btnXoa.BackColor = SystemColors.ButtonFace;
+            btnXoa.Location = new Point(413, 287);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(94, 29);
+            btnXoa.TabIndex = 17;
+            btnXoa.Text = "Xóa Chọn";
+            btnXoa.UseVisualStyleBackColor = false;
+            btnXoa.Click += btnXoa_Click;
+            // 
+            // btnLuu
+            // 
+            btnLuu.BackColor = SystemColors.ButtonFace;
+            btnLuu.Location = new Point(254, 287);
+            btnLuu.Name = "btnLuu";
+            btnLuu.Size = new Size(136, 29);
+            btnLuu.TabIndex = 16;
+            btnLuu.Text = "Lưu Thông Tin";
+            btnLuu.UseVisualStyleBackColor = false;
+            btnLuu.Click += btnLuu_Click;
+            // 
+            // dGVThongTin
+            // 
+            dGVThongTin.AllowUserToAddRows = false;
+            dGVThongTin.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dGVThongTin.Columns.AddRange(new DataGridViewColumn[] { MaSV, HoTen, ChuyenNganh, GioiTinh, SoMon, MonHoc });
+            dGVThongTin.Location = new Point(0, 328);
+            dGVThongTin.Name = "dGVThongTin";
+            dGVThongTin.ReadOnly = true;
+            dGVThongTin.RowHeadersWidth = 51;
+            dGVThongTin.ScrollBars = ScrollBars.Horizontal;
+            dGVThongTin.Size = new Size(788, 188);
+            dGVThongTin.TabIndex = 1;
+            // 
+            // MaSV
+            // 
+            MaSV.HeaderText = "MSSV";
+            MaSV.MinimumWidth = 6;
+            MaSV.Name = "MaSV";
+            MaSV.ReadOnly = true;
+            MaSV.Width = 125;
+            // 
+            // HoTen
+            // 
+            HoTen.HeaderText = "Họ Tên";
+            HoTen.MinimumWidth = 6;
+            HoTen.Name = "HoTen";
+            HoTen.ReadOnly = true;
+            HoTen.Width = 200;
+            // 
+            // ChuyenNganh
+            // 
+            ChuyenNganh.HeaderText = "Chuyên Ngành";
+            ChuyenNganh.MinimumWidth = 6;
+            ChuyenNganh.Name = "ChuyenNganh";
+            ChuyenNganh.ReadOnly = true;
+            ChuyenNganh.Width = 150;
+            // 
+            // GioiTinh
+            // 
+            GioiTinh.HeaderText = "Giới Tính";
+            GioiTinh.MinimumWidth = 6;
+            GioiTinh.Name = "GioiTinh";
+            GioiTinh.ReadOnly = true;
+            GioiTinh.Width = 125;
+            // 
+            // SoMon
+            // 
+            SoMon.HeaderText = "Số Môn";
+            SoMon.MinimumWidth = 6;
+            SoMon.Name = "SoMon";
+            SoMon.ReadOnly = true;
+            SoMon.Width = 125;
+            // 
+            // MonHoc
+            // 
+            MonHoc.HeaderText = "Danh Sách Môn Học";
+            MonHoc.MinimumWidth = 6;
+            MonHoc.Name = "MonHoc";
+            MonHoc.ReadOnly = true;
+            MonHoc.Width = 200;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 564);
+            Controls.Add(dGVThongTin);
+            Controls.Add(groupBoxThongTin);
+            Name = "Form1";
+            Text = "Nhập Liệu Sinh Viên";
+            Load += Form1_Load;
+            groupBoxThongTin.ResumeLayout(false);
+            groupBoxThongTin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dGVThongTin).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox txtMaSV;
+        private TextBox txtHoTen;
+        private ComboBox cbChuyenNganh;
+        private ListBox listBoxMonHoc1;
+        private ListBox listBoxMonHoc2;
+        private Button btnChuyenPhai;
+        private Button btnChuyenTrai;
+        private GroupBox groupBoxThongTin;
+        private Button btnXoa;
+        private Button btnLuu;
+        private DataGridView dGVThongTin;
+        private RadioButton rbNu;
+        private RadioButton rbNam;
+        private DataGridViewTextBoxColumn MaSV;
+        private DataGridViewTextBoxColumn HoTen;
+        private DataGridViewTextBoxColumn ChuyenNganh;
+        private DataGridViewTextBoxColumn GioiTinh;
+        private DataGridViewTextBoxColumn SoMon;
+        private DataGridViewTextBoxColumn MonHoc;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace BT09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBoxThongTin_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMaSV.Text))
+            {
+                MessageBox.Show("Vui lòng nhập Mã số sinh viên!");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtHoTen.Text))
+            {
+                MessageBox.Show("Vui lòng nhập Họ Tên!");
+                return;
+            }
+            if (cbChuyenNganh.SelectedIndex < 0)
+            {
+                MessageBox.Show("Vui lòng chọn Chuyên Ngành!");
+                return;
+            }
+
+            string gt = "";
+            if (rbNam.Checked) gt = "Nam";
+            else if (rbNu.Checked) gt = "Nữ";
+            else
+            {
+                MessageBox.Show("Vui lòng chọn Giới tính!");
+                return;
+            }
+           
+            int soMon = listBoxMonHoc2.Items.Count;
+            dGVThongTin.Rows.Add(txtMaSV.Text, txtHoTen.Text, cbChuyenNganh.SelectedItem.ToString(), gt, soMon);
+            txtMaSV.Clear();
+            txtHoTen.Clear();
+            cbChuyenNganh.SelectedIndex = -1;
+            rbNam.Checked = false;
+            rbNu.Checked = false;
+            foreach (var item in listBoxMonHoc2.Items)
+                listBoxMonHoc1.Items.Add(item);
+            listBoxMonHoc2.Items.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Enabled = true;
+            txtMaSV.Clear();
+            txtHoTen.Clear();
+            rbNam.Checked = false;
+            rbNu.Checked = false;
+            dGVThongTin.AllowUserToAddRows = false;
+            cbChuyenNganh.Items.Add("Kỹ Thuật Phần Mềm");
+            cbChuyenNganh.Items.Add("Kỹ Thuật Máy Tính");
+            cbChuyenNganh.Items.Add("Khoa Học Máy Tính");
+            cbChuyenNganh.Items.Add("Trí Tuệ Nhân Tạo");
+            cbChuyenNganh.Items.Add("Thương Mại Điện Tử");
+            cbChuyenNganh.Items.Add("Truyền Thông Đa Phương Tiện");
+            cbChuyenNganh.Items.Add("An Toàn Thông Tin");
+            cbChuyenNganh.Items.Add("Hệ Thống Thông Tin");
+            cbChuyenNganh.Items.Add("Mạng Máy Tính và Truyền Thông");
+            cbChuyenNganh.Items.Add("Công Nghệ Thông Tin");
+            cbChuyenNganh.Items.Add("Khoa Học Dữ Liệu");
+            if (cbChuyenNganh.Items.Count > 0)
+                cbChuyenNganh.SelectedIndex = 0;
+
+            listBoxMonHoc1.Items.Clear();
+            listBoxMonHoc1.SelectionMode = SelectionMode.MultiExtended;
+            listBoxMonHoc1.Items.Add("Giải tích");
+            listBoxMonHoc1.Items.Add("Nhập môn lập trình");
+            listBoxMonHoc1.Items.Add("Đại số tuyến tính");
+            listBoxMonHoc1.Items.Add("Lập trình trực quan");
+            listBoxMonHoc1.Items.Add("Cơ sở dữ liệu");
+            listBoxMonHoc1.Items.Add("Mạng máy tính");
+            listBoxMonHoc1.Items.Add("Hệ điều hành");
+            listBoxMonHoc1.Items.Add("AI cơ bản");
+            listBoxMonHoc1.Items.Add("Trí tuệ nhân tạo");
+            listBoxMonHoc1.Items.Add("Nhập môn mạng máy tính");
+
+            listBoxMonHoc2.Items.Clear();
+
+        }
+        private void btnChuyenPhai_Click(object sender, EventArgs e)
+        {
+            var selected = new object[listBoxMonHoc1.SelectedItems.Count];
+            listBoxMonHoc1.SelectedItems.CopyTo(selected, 0);
+
+            foreach (var item in selected)
+            {
+                listBoxMonHoc1.Items.Remove(item);
+                listBoxMonHoc2.Items.Add(item);
+            }
+        }
+        private void btnChuyenTrai_Click(object sender, EventArgs e)
+        {
+            var selected = new object[listBoxMonHoc2.SelectedItems.Count];
+            listBoxMonHoc2.SelectedItems.CopyTo(selected, 0);
+
+            foreach (var item in selected)
+            {
+                listBoxMonHoc2.Items.Remove(item);
+                listBoxMonHoc1.Items.Add(item);
+            }
+        }
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (dGVThongTin.SelectedRows.Count > 0)
+            {
+                DialogResult result = MessageBox.Show(
+                    "Bạn có chắc chắn muốn xóa dòng này?",
+                    "Xác nhận xóa",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question
+                );
+
+                if (result == DialogResult.Yes)
+                {
+                    foreach (DataGridViewRow row in dGVThongTin.SelectedRows)
+                        dGVThongTin.Rows.Remove(row);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một dòng để xóa!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+    }
+
+}
